fbadac4e786d491426a4231b81ccc0476671015e
David Nguyen <davidnguyen21@gmail.com>
2016-04-05 12:21:49 +0900

    added all buttons, working on factory

--- public/app.js
+++ public/app.js
@@ -20,18 +20,46 @@ angular.module('app', [
         controller: 'main'
       })
       .otherwise({ redirectTo: '/' })
-
-
-
   })
+  .controller('main', function($scope, $location, Mather) {
 
-.controller('main', function($scope, $location) {
+    //angular.extend($scope, Mather);
+    $scope.money = 0;
 
-    $scope.isActive = function (viewLocation) { 
-        return viewLocation === $location.path();
+
+    
+
+    $scope.isActive = function(viewLocation) {
+      return viewLocation === $location.path();
     };
 
+    $scope.add = function() {
+      Mather.make();
+      $scope.money = Mather.wallet;
+      console.log("mather make  "+ JSON.stringify(Mather))
+    };
 
+  })
+  .factory('Mather', function() {
+    var donuts = 0;
+    var wallet = 0;
+    var brooms = 0;
 
+    var make = function() {
+      donuts++;
+      console.log("donuts " + donuts)
+      var ten = donuts / 10;
+      if (Number.isInteger(ten)) {
+        wallet++;
+        console.log("wallet: " + wallet)
+       // return wallet;
+      }
+    };
 
-})
+    return {
+      donuts: donuts,
+      wallet: wallet,
+      brooms: brooms,
+      make: make
+    }
+  })
